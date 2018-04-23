acf626e2c66de7f9b6807dac81ebfd394fe3f019
Robert Schaefer <git@roschaefer.de>
2018-04-23 23:05:48 +0200

    Setup cucumber-js
    All step definitions pending

---
+++ features/step_definitions/steps.js
@@ -0,0 +1,26 @@
+const { Given, When, Then } = require('cucumber');
+
+Given('there is an organization in Human Connection with these credentials:', function (dataTable) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Given('I am authenticated', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('I send a POST request to {string} with:', function (string, docString) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('there is an access token in the response:', function (docString) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('a new post should be created', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
